e64f1ad7a70a44252ef0d793f5bb4c077067ef6e
Robson Nonato <108809490+NonattoDev@users.noreply.github.com>
2023-06-20 13:20:50 -0300

    Alt

--- backend/src/bancodedados/conexao.ts
+++ backend/src/bancodedados/conexao.ts
@@ -2,13 +2,7 @@ import conexao from "knex";
 
 export const knex = conexao({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PSDW,
-    database: process.env.DB_DATABASE,
-  },
+  connection: process.env.INTERNAL_DATABASE_URL,
 });
 
 async function createColaboradoresTable() {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import rotas from "./rotas/rotas";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
